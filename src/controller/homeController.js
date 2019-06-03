84ebdc94b879c1ad49ca1e0b414c83e7d691b781
advancevillage <cugriver@gmail.com>
2019-06-03 23:57:31 +0800

    #richard: 添加商品显示

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -7,7 +7,7 @@ const Wrapper =  async  (cat_id,page_id) => {
     const params = {};
     params.title    = await hm.TitleProcessor();
     params.navigate = await hm.NavigateProcessor(cat_id,page_id);
-    params.goods    = await hm.ListGoodsProcessor();
+    params.goods    = await hm.ListGoodsProcessor(cat_id,page_id);
     return params;
 };
 
--- src/service/homeService.js
+++ src/service/homeService.js
@@ -24,11 +24,21 @@ const NavigateProcessor = async (cat_id,page_id) => {
     return ret.data.navigate
 };
 
-const ListGoodsProcessor = async () => {
-    return [{title:"文章1"},{title:"文章2"},{title:"文章2"},{title:"文章2"},{title:"文章2"},
-            {title:"文章2"},{title:"文章2"},{title:"文章2"},{title:"文章2"},{title:"文章2"},
-            {title:"文章2"},{title:"文章2"},{title:"文章2"},{title:"文章2"},{title:"文章2"},
-            {title:"文章2"},{title:"文章2"},{title:"文章2"},{title:"文章2"},{title:"文章2"}];
+const ListGoodsProcessor = async (cat_id,page_id) => {
+    const options = {
+        uri : "http://localhost:13147/",
+        method: 'GET',
+        qs: {
+            cat: cat_id,
+            page: page_id
+        },
+        headers: [
+            'content-type: application/json; charset=utf-8'
+        ]
+    };
+    let response =  await  RequestService(options);
+    let ret = JSON.parse(response.body);
+    return ret.data.goods
 };
 
 module.exports = {
