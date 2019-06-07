353c869388bc9cd33e00d38a352cc6ad5d23bd8c
advancevillage <cugriver@gmail.com>
2019-06-07 23:50:37 +0800

    #richard: 颜色系列

---
+++ src/static/js/goods.js
@@ -0,0 +1,28 @@
+//@author: richard
+const goods_menu_colors_show_canvas = "goods_menu_colors_show_canvas";
+const ClassSelector = ".";
+
+function ShowGoodsColors() {
+    $(ClassSelector + goods_menu_colors_show_canvas)
+        .drawArc({
+            fillStyle: '#36b',
+            strokeWidth: 0,
+            x: 15, y: 15,
+            radius: 12,
+            ccw: true,
+            inDegrees: false
+
+        })
+        .drawLine({
+            strokeStyle: '#fff',
+            strokeWidth: 2,
+            x1: 9,  y1: 16,
+            x2: 13, y2: 20,
+            x3: 22, y3: 12,
+        });
+}
+
+ShowGoodsColors();
+
+
+
