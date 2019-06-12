d3d90f0f91931905d65020d6fdbed1ef811deca2
advancevillage <cugriver@gmail.com>
2019-06-12 23:14:08 +0800

    #richard: 添加颜色圈动态效果

--- src/static/js/goods.js
+++ src/static/js/goods.js
@@ -5,28 +5,17 @@ const ClassSelector = ".";
 function ShowGoodsColors() {
     $(ClassSelector + goods_menu_colors_show_canvas).each(function (index, value) {
         let color = $(value).attr("data-color");
-        SetGoodsColors(ClassSelector + goods_menu_colors_show_canvas + index, color)
+        //设置颜色圈
+        $(ClassSelector + goods_menu_colors_show_canvas + index).css({"background":color});
+        //设置鼠标进入和离开事件
+        $(ClassSelector + goods_menu_colors_show_canvas + index).mouseenter(function () {
+            $(ClassSelector + goods_menu_colors_show_canvas + index).css({"width":"30px", "height":"30px","margin-top":0,"margin-left":0})
+        });
+        $(ClassSelector + goods_menu_colors_show_canvas + index).mouseleave(function () {
+            $(ClassSelector + goods_menu_colors_show_canvas + index).css({"width":"24px", "height":"24px","margin-top":"3px","margin-left":"3px"})
+        });
+
     });
-}
-
-function SetGoodsColors(key, color) {
-    $(key)
-        .drawArc({
-            fillStyle: color,
-            strokeWidth: 0,
-            x: 15, y: 15,
-            radius: 11,
-            ccw: true,
-            inDegrees: false
-
-        })
-        // .drawLine({
-        //     strokeStyle: '#fff',
-        //     strokeWidth: 2,
-        //     x1: 9,  y1: 16,
-        //     x2: 13, y2: 20,
-        //     x3: 22, y3: 12,
-        // });
 }
 
 ShowGoodsColors();
