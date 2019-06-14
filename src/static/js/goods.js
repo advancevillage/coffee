ae757ae7261e159d0676c35592bdbda8a1c454d5
advancevillage <cugriver@gmail.com>
2019-06-14 16:22:50 +0800

    #richard: 添加尺码样式

--- src/static/js/goods.js
+++ src/static/js/goods.js
@@ -1,8 +1,10 @@
 //@author: richard
 const goods_menu_colors_show_canvas = "goods_menu_colors_show_canvas";
+const goods_container_detail_attr_size_label = "goods_container_detail_attr_size_label";
 const ClassSelector = ".";
 
 function ShowGoodsColors() {
+    //颜色属性设置
     $(ClassSelector + goods_menu_colors_show_canvas).each(function (index, value) {
         let color = $(value).attr("data-color");
         //设置颜色圈
@@ -15,6 +17,15 @@ function ShowGoodsColors() {
             $(ClassSelector + goods_menu_colors_show_canvas + index).css({"width":"24px", "height":"24px","margin-top":"3px","margin-left":"3px"})
         });
 
+    });
+    //尺码设置
+    $(ClassSelector + goods_container_detail_attr_size_label).each(function (index, value) {
+        $(ClassSelector + goods_container_detail_attr_size_label + index).mouseenter(function () {
+            $(ClassSelector + goods_container_detail_attr_size_label + index).css({"opacity":1,"border":"1px solid black"})
+        });
+        $(ClassSelector + goods_container_detail_attr_size_label + index).mouseleave(function () {
+            $(ClassSelector + goods_container_detail_attr_size_label + index).css({"opacity":0.5,"border":"1px solid grey"})
+        })
     });
 }
 
